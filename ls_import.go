2bc0cab404f19c711c1c8fd31b30cfd04efaf936
Tom Harrison <tomharrison@gmail.com>
2014-08-12 19:22:15 -0400

    Refactoring and documentation

--- ls_import.go
+++ ls_import.go
@@ -1,3 +1,7 @@
+// ls_import is a utility which downloads diet data from one's Livestrong diary
+// and stores it in MongoDB. Livestrong unfortunately doesn't have much of a
+// public API, but we can still use the net/http library to automate logging
+// into one's account and using the "Export CSV" feature of the Diary page.
 package main
 
 import (
@@ -15,17 +19,24 @@ import (
 	"time"
 )
 
+// Names of the arguments which may be given as command line parameters.
 const (
-	DefaultEndDate   string = "2012-11-01"
-	DefaultPassword  string = ""
-	DefaultStartDate string = "2012-11-30"
-	DefaultUsername  string = "tomharrison"
 	EndDateFlag      string = "end"
 	PasswordFlag     string = "passwd"
 	StartDateFlag    string = "start"
 	UsernameFlag     string = "uname"
 )
 
+// Default values for all of the command line parameters.
+const (
+	DefaultEndDate   string = "2012-11-01"
+	DefaultPassword  string = ""
+	DefaultStartDate string = "2012-11-30"
+	DefaultUsername  string = "tomharrison"
+)
+
+// Options is a struct which encapsulates the settings which may be given
+// as command line arguments.
 type Options struct {
 	EndDate   string
 	Password  string
@@ -34,33 +45,37 @@ type Options struct {
 }
 
 func main() {
+	// Parse command line options.
 	opts := getOptions()
 	dbOpts := fit.ParseMongoOptions()
+	start := parseDateOption(opts.StartDate)
+	end := parseDateOption(opts.EndDate)
 
+	// Initialize dependencies.
 	cookieJar, _ := cookiejar.New(nil)
 	client := &http.Client{Jar: cookieJar}
+	mongo := getMongo(dbOpts)
+	entries := mongo.DB(dbOpts.Database).C("entries")
+	repo := fit.NewEntryRepository(entries)
 
+	// Download and store data.
 	login(opts.Username, opts.Password, client)
-
-	start, end := parseDateOptions(opts)
-	dietResp, dietErr := requestDietData(&start, &end, client)
-	if dietErr != nil {
-		panic(dietErr)
-	}
-
-	_, collection := getMongo(dbOpts)
-	repo := fit.NewEntryRepository(collection)
+	dietResp := requestDietData(&start, &end, client)
 	storeDietData(dietResp, repo)
 }
 
-func getMongo(opts *fit.MongoOptions) (*mgo.Session, *mgo.Collection) {
+// Connect to MongoDB.
+func getMongo(opts *fit.MongoOptions) *mgo.Session {
 	session, err := mgo.Dial(opts.Host)
 	if err != nil {
 		panic(err)
 	}
-	return session, session.DB(opts.Database).C("entries")
+	return session
 }
 
+// getOptions parses the command line parameters and returns their values
+// inside an Options struct. Any argument which is not given will be
+// initialized to a default value, which is defined above as a constant.
 func getOptions() *Options {
 	opts := &Options{}
 
@@ -73,12 +88,20 @@ func getOptions() *Options {
 	return opts
 }
 
-func parseDateOptions(opts *Options) (time.Time, time.Time) {
-	start, _ := time.Parse("2006-01-02", opts.StartDate)
-	end, _ := time.Parse("2006-01-02", opts.EndDate)
-	return start, end
+// Parse a given date option from a string into an instance of time.Time. Dates
+// must be formatted like 2006-01-02.
+func parseDateOption(dateStr string) time.Time {
+	d, err := time.Parse("2006-01-02", dateStr)
+	if err != nil {
+		panic(err)
+	}
+	return d
 }
 
+// Log into Livestrong.com with the given username and password, via the given
+// http client.
+//
+// Precondition: the given http client has a cookie jar.
 func login(username string, password string, c *http.Client) {
 	params := make(url.Values)
 	params.Set("login_user", username)
@@ -92,7 +115,11 @@ func login(username string, password string, c *http.Client) {
 	}
 }
 
-func requestDietData(start *time.Time, end *time.Time, c *http.Client) (*http.Response, error) {
+// Request diet data logged between the given start and end times, via the given
+// http client. The response's payload will be CSV values each containing a date,
+// calorie goal, gross calories consumed, total calories burned, and a net calorie
+// count (gross minus burned).
+func requestDietData(start *time.Time, end *time.Time, c *http.Client) *http.Response {
 	params := make(url.Values)
 	params.Set("start_Month", start.Format("01"))
 	params.Set("start_Day", start.Format("02"))
@@ -103,9 +130,15 @@ func requestDietData(start *time.Time, end *time.Time, c *http.Client) (*http.Re
 	params.Set("ftype", "overview")
 	params.Set("fltype", "csv")
 
-	return c.PostForm("http://www.livestrong.com/thedailyplate/diary/csv/tomharrison/", params)
+	resp, err := c.PostForm("http://www.livestrong.com/thedailyplate/diary/csv/tomharrison/", params)
+	if err != nil {
+		panic(err)
+	}
+	return resp
 }
 
+// storeDietData parses CSV data from the given http response's body into 
+// Entry records and stores them in MongoDB via the given repository.
 func storeDietData(r *http.Response, repo *fit.EntryRepository) {
 	defer r.Body.Close()
 	reader := csv.NewReader(r.Body)
@@ -130,6 +163,8 @@ func storeDietData(r *http.Response, repo *fit.EntryRepository) {
 	}
 }
 
+// newEntryFromRecord is a factory function which intializes the Entry type
+// using an array of strings which were parsed from a CSV record.
 func newEntryFromRecord(r []string) *fit.Entry {
 	e := &fit.Entry{}
 	reg := regexp.MustCompile("[a-z]{2},")
